e92a46b6735d382f2bdea0ec528246c9bcf6a50c
eliteteam743 <56851693+eliteteam743@users.noreply.github.com>
2019-11-10 18:30:14 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="7-pilihan-makanan-sehat-guna-jantung.html"}
-if(x=="2"){xxx="5-cara-membuat-salad-buah-yang-menarik.html"}
-if(x=="3"){xxx="ketahui-cara-membuat-omelet-prancis.html"}
-if(x=="4"){xxx="5-trik-memanggang-tanpa-oven-yang-harus"}a_to_ve[i].href="https://www.greatcooking.me/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
+if(x=="2"){xxx="practice-environmental-qwareness-to-our.html"}
+if(x=="3"){xxx="10-tips-for-start-small-business-startup.html"}
+if(x=="4"){xxx="advantages-of-being-home-business-owner.html"}a_to_ve[i].href="https://www.greatcooking.me/2019/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
